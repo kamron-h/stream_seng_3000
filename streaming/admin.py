a5422d3b90733d6042aec4bc0ff9f701623c3a60
Kamron <kamronthopkins@gmail.com>
2023-10-09 22:33:37 -0400

    Commit 4

--- streaming/admin.py
+++ streaming/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from streaming.models import Video, Actor, Rating
 
 # Register your models here.
+admin.site.register(Video)
+admin.site.register(Actor)
+admin.site.register(Rating)
+
--- streaming/urls.py
+++ streaming/urls.py
@@ -1,7 +1,4 @@
-"""demo URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+"""
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +10,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from app import views
+from django.urls import path, include
 from streaming import views
 
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
+    path('', views.home, name='stream_dash'),
+    path('stream/', views.stream_dash, name='stream_dash'),
+    path('index/', views.api_index, name='api_index'),
+    path('api/', include('streaming.urls_api')),
 ]
