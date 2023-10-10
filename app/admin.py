998cff585a9e6155db1ad320f68a950eb308e77a
Kamron <kamronthopkins@gmail.com>
2023-10-09 22:22:54 -0400

    Commit 3

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+# from .models import Movie, Comment, Genre, Actor, Director, MovieImage, Rating
+#
+# admin.site.register(Movie)
+# admin.site.register(Comment)
+# admin.site.register(Genre)
+# admin.site.register(Actor)
+# admin.site.register(Director)
+# admin.site.register(MovieImage)
+# admin.site.register(Rating)
+
--- streaming/urls.py
+++ streaming/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from app import views
+from streaming import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    path('', include('streaming.urls')),
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
